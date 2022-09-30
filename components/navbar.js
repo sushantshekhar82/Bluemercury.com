a509e99b384b0d9ab36f96d852162462e63f9c84
dcs1997 <dhirajsalvi099@gmail.com>
2022-09-30 11:23:13 +0530

    day-2

---
+++ components/navbar.js
@@ -0,0 +1,51 @@
+let footer=()=>{
+return ` <div>
+<h5>Get Expert Beauty Insight & Offers</h5>
+<input type="text" placeholder="Email Address">
+<p>By continuing, you agree to Bluemercury's Privacy Practices.</p>
+<i class="fa fa-twitter" aria-hidden="true"></i>
+<i class="fa fa-pinterest" aria-hidden="true"></i>
+<i class="fa fa-facebook-square"aria-hidden="true"></i>
+<i class="fa fa-instagram" aria-hidden="true"></i>
+<i class="fa fa-youtube-play"></i>
+<p>Copyright 2022 bluemercury. All Rights Reserved.</p>
+</div>
+<div>
+<h3>BLUEMERCURY</h3>
+<p>Our Company </p>
+<p>Store Locator </p>
+<p>In-Store & Online Events </p>
+<p>Spa Menu </p>
+<p>Careers</p>
+<p>Vendor Submissions</p>
+<p>Diversity & Inclusion</p>
+</div>
+
+<div> <h3>MY ACCOUNT</h3>
+<p>My Account</p>
+<p>BlueRewards </p>
+<p>Gift Card Balance Checker </p>
+</div>
+
+<div> 
+<h3>CUSTOMER SERVICE</h3>
+<p>Contact Us</p>
+<p>Help & FAQ</p>
+<p>Shipping, Returns & Exchanges</p>
+<p>Gift Cards</p>
+<p>Offers & Promotions</p>
+<p>Klarna</p>
+</div>
+
+<div>
+<h3>RESOURCES</h3>
+<p>Terms & Conditions</p>
+<p>Privacy Notice</p>
+<p>Do Not Sell My Personal Information</p>
+<p>CA Privacy Rights</p>
+<p>Accessibility</p>
+<p>Affiliate Program</p>
+</div>`
+}
+
+ export  {footer}
