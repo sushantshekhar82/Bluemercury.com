27608f1381b9a953d801ef13c39f7fb9f4a88fa7
RoshaniChouhan <101568343+RoshaniChouhan@users.noreply.github.com>
2022-10-01 22:39:56 +0530

    login signup final

---
+++ login.js
@@ -0,0 +1,44 @@
+let verifyUser = (newemail, newpassword) => {
+
+  let signupData = JSON.parse(localStorage.getItem("signupData"));
+  for (let ele of signupData) {
+    let { email, password } = ele;
+    if (email == newemail && newpassword == password) {
+      alert("Login successful!");
+
+      return true;
+    } else if (email == newemail && newpassword != password) {
+      alert("Wrong credentials");
+      return false;
+    }
+  }
+
+  alert("User doesn't exist, Sign Up");
+  return false;
+};
+
+let signupData = JSON.parse(localStorage.getItem("signupData"));
+let login = localStorage.getItem("login") || false;
+
+function singData(e, p) {
+  (this.email = e), (this.password = p);
+}
+
+function sigin(e) {
+  // e.preventDefault();
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  // let d=new singData(email,password)
+  // console.log(d)
+  // console.log(d.email)
+  let emai = (document.getElementById("email").value = "");
+  let pass = (document.getElementById("password").value = "");
+
+  if (verifyUser(email, password) === true) {
+    localStorage.setItem("login", true);
+    window.location.href = "index.html";
+  } else {
+    localStorage.setItem("login", false);
+    return;
+  }
+}
