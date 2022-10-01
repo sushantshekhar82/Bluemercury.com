6a86cf0bcffea3dbc9972de967d764cf359e9df8
dcs1997 <dhirajsalvi099@gmail.com>
2022-10-01 07:30:22 +0530

    day-3

--- components/navbar.js
+++ components/navbar.js
@@ -48,4 +48,171 @@ return ` <div>
 </div>`
 }
 
- export  {footer}
+let navbar=()=>{
+    return ` <div id="upper">
+    <div id="left">
+        <i class="fa fa-map-marker" aria-hidden="true">STORE & SPA LOCATOR</i>
+    </div>
+    <div id="upper_middle">
+        <img
+            src="https://cdn.shopify.com/s/files/1/0283/0185/2747/files/bluemercury-logo_1216x@2x.png?v=1648743182">
+    </div>
+    <div id="right">
+        <i class="fa fa-heart" aria-hidden="true"> wishlist</i>
+        <i class="fa fa-search" aria-hidden="true"> Search</i>
+        <i class="fa fa-user" aria-hidden="true">Sign in/up</i>
+        <i class="fa fa-shopping-bag" aria-hidden="true"> Bag</i>
+
+    </div>
+
+</div>
+
+<!-- upper part ends here -->
+
+<!-- middle part starts here -->
+<div id="middle">
+    <ul>
+        <li><a href="#">SHOP</a>
+            <ul>
+                <li><a href="#">SKIN CARE</a></li>
+                <li><a href="#">MAKE UP</a></li>
+                <li><a href="#">HAIR</a></li>
+                <li><a href="#">BATH AND BODY</a></li>
+                <li><a href="#">FRAGRANCE</a></li>
+                <li><a href="#">HOME AND LIFESTYLE</a></li>
+                <li><a href="#">SUN CARE</a></li>
+                <li><a href="#">MEN</a></li>
+                <li><a href="#">GIFTS</a></li>
+                <li><a href="#">BEST SELLER</a></li>
+                <li><a href="#">WELLNESS</a></li>
+                <li><a href="#"> FREE GIFTS!</a></li>
+
+            </ul>
+        </li>
+
+
+        <li><a href="#">NEW</a>
+            <ul>
+                <li><a href="#">SKIN CARE</a></li>
+                <li><a href="#">MAKE UP</a></li>
+                <li><a href="#">HAIR</a></li>
+                <li><a href="#">BATH AND BODY</a></li>
+                <li><a href="#">FRAGRANCE</a></li>
+                <li><a href="#">HOME AND LIFESTYLE</a></li>
+                <li><a href="#">SUN CARE</a></li>
+                <li><a href="#">MEN</a></li>
+                <li><a href="#">GIFTS</a></li>
+                <li><a href="#">BEST SELLER</a></li>
+                <li><a href="#">WELLNESS</a></li>
+                <li><a href="#"> FREE GIFTS!</a></li>
+
+            </ul>
+        </li>
+
+
+        <li><a href="#">BRANDS</a>
+            <ul>
+                <li><a href="#">SKIN CARE</a></li>
+                <li><a href="#">MAKE UP</a></li>
+                <li><a href="#">HAIR</a></li>
+                <li><a href="#">BATH AND BODY</a></li>
+                <li><a href="#">FRAGRANCE</a></li>
+                <li><a href="#">HOME AND LIFESTYLE</a></li>
+                <li><a href="#">SUN CARE</a></li>
+                <li><a href="#">MEN</a></li>
+                <li><a href="#">GIFTS</a></li>
+                <li><a href="#">BEST SELLER</a></li>
+                <li><a href="#">WELLNESS</a></li>
+                <li><a href="#"> FREE GIFTS!</a></li>
+
+            </ul>
+        </li>
+
+
+
+        <li><a href="#">EXPLORE</a>
+            <ul>
+                <li><a href="#">SKIN CARE</a></li>
+                <li><a href="#">MAKE UP</a></li>
+                <li><a href="#">HAIR</a></li>
+                <li><a href="#">BATH AND BODY</a></li>
+                <li><a href="#">FRAGRANCE</a></li>
+                <li><a href="#">HOME AND LIFESTYLE</a></li>
+                <li><a href="#">SUN CARE</a></li>
+                <li><a href="#">MEN</a></li>
+                <li><a href="#">GIFTS</a></li>
+                <li><a href="#">BEST SELLER</a></li>
+                <li><a href="#">WELLNESS</a></li>
+                <li><a href="#"> FREE GIFTS!</a></li>
+
+            </ul>
+        </li>
+
+
+
+        <li><a href="#">EVENTS</a>
+            <ul>
+                <li><a href="#">SKIN CARE</a></li>
+                <li><a href="#">MAKE UP</a></li>
+                <li><a href="#">HAIR</a></li>
+                <li><a href="#">BATH AND BODY</a></li>
+                <li><a href="#">FRAGRANCE</a></li>
+                <li><a href="#">HOME AND LIFESTYLE</a></li>
+                <li><a href="#">SUN CARE</a></li>
+                <li><a href="#">MEN</a></li>
+                <li><a href="#">GIFTS</a></li>
+                <li><a href="#">BEST SELLER</a></li>
+                <li><a href="#">WELLNESS</a></li>
+                <li><a href="#"> FREE GIFTS!</a></li>
+
+            </ul>
+        </li>
+
+
+        <li><a href="#">BLUE REWARD</a>
+            <ul>
+                <li><a href="#">SKIN CARE</a></li>
+                <li><a href="#">MAKE UP</a></li>
+                <li><a href="#">HAIR</a></li>
+                <li><a href="#">BATH AND BODY</a></li>
+                <li><a href="#">FRAGRANCE</a></li>
+                <li><a href="#">HOME AND LIFESTYLE</a></li>
+                <li><a href="#">SUN CARE</a></li>
+                <li><a href="#">MEN</a></li>
+                <li><a href="#">GIFTS</a></li>
+                <li><a href="#">BEST SELLER</a></li>
+                <li><a href="#">WELLNESS</a></li>
+                <li><a href="#"> FREE GIFTS!</a></li>
+
+            </ul>
+        </li>
+
+        <li><a href="#">ROUTINR REBOOT</a>
+            <ul>
+                <li><a href="#">SKIN CARE</a></li>
+                <li><a href="#">MAKE UP</a></li>
+                <li><a href="#">HAIR</a></li>
+                <li><a href="#">BATH AND BODY</a></li>
+                <li><a href="#">FRAGRANCE</a></li>
+                <li><a href="#">HOME AND LIFESTYLE</a></li>
+                <li><a href="#">SUN CARE</a></li>
+                <li><a href="#">MEN</a></li>
+                <li><a href="#">GIFTS</a></li>
+                <li><a href="#">BEST SELLER</a></li>
+                <li><a href="#">WELLNESS</a></li>
+                <li><a href="#"> FREE GIFTS!</a></li>
+
+            </ul>
+        </li>
+
+
+
+    </ul>
+</div>
+
+
+
+<div id="bottom">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, dolore.</div>`
+}
+
+ export  {footer,navbar}
